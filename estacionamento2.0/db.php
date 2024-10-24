e1e725bf95788ce91ac69621d40d6d617fde5c48
Enrico Tondato <160749339+AshesOfBjorn@users.noreply.github.com>
2024-10-24 15:18:42 -0300

    Add files via upload

---
+++ estacionamento2.0/db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; // ou o endereço do seu servidor
+$username = "root"; // seu nome de usuário do MySQL
+$password = ""; // sua senha do MySQL
+$dbname = "estacionamento"; // nome do seu banco de dados
+
+// Cria a conexão
+$conexao = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica a conexão
+if ($conexao->connect_error) {
+    die("Conexão falhou: " . $conexao->connect_error);
+}
+?>
